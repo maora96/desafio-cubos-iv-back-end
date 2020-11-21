44927b745c5eb94abfa460ff79004a4486c64261
Marina Oliveira <marinaoliveira.r07@gmail.com>
2020-11-20 22:53:17 -0300

    template para o projeto implementado.

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const Router = require('koa-router');
+const router = new Router();
+
+
+// get
+router.get();
+
+// post
+router.post();
+
+// put
+router.put();
+
+// delete
+router.delete();
+
+module.exports = router;
